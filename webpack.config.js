31218030bfc299a092d65cdcfe5e22fa85295d1f
davidchuang5 <davidchuang5@gmail.com>
2023-11-02 16:39:08 -0500

    Changed outpath to dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,45 @@
-const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: process.env.NODE_ENV,
-  entry: './client/index.js',
+  entry: "./client/index.js",
   output: {
-    path: path.join(__dirname, './client/index.html'),
-    filename: 'bundle.js'
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
   },
-  
+
   module: {
     rules: [
       {
         test: /.(js|jsx)$/, //telling it to bundle all .js files
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        } 
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
       },
       {
         test: /.(css|scss)$/,
         exclude: /node_modules/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      }
-    ]
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
   },
   devServer: {
     static: {
-      publicPath: '/',
-      directory: path.resolve(__dirname, '/client'),
+      publicPath: "/",
+      directory: path.resolve(__dirname, "/client"),
     },
     proxy: {
-      '/api/search': 'http://localhost:3000',
+      "/api/search": "http://localhost:3000",
     },
   },
   plugins: [
     new HTMLWebpackPlugin({
-      template: './client/index.html'
-    })
+      template: "./client/index.html",
+    }),
   ],
-}
+};
